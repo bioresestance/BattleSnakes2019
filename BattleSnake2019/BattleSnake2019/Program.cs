afe36206c3d3c629cc4ed78c221f7bc3003f3e0e
Aaron Bromma <aaron.bromma@gmail.com>
2019-02-16 20:06:19 -0800

    First Commit of code.
                                              * Added HTTP server to listen to requests.

---
+++ BattleSnake2019/BattleSnake2019/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace BattleSnake2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Webserver ws = new Webserver( "http://localhost:8080/", SendResponse);
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+ 
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            Console.WriteLine("Request type: {0}", request.HttpMethod );
+
+            //Webserver.DisplayWebHeaderCollection(request);
+            
+            // Get the data from the HTTP stream
+            var body = new StreamReader(request.InputStream).ReadToEnd();
+            
+            Console.WriteLine("Body:  {0}", body);
+            
+            
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);    
+        }
+    }
+}
---
+++ BattleSnake2019/BattleSnake2019/webserver.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Linq;
+using System.Text;
+ 
+namespace BattleSnake2019
+{
+    
+    // Web serer code based on code found at https://codehosting.net/blog/BlogEngine/post/Simple-C-Web-Server.
+    
+    public class Webserver
+    {
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+ 
+        public Webserver(string address, Func<HttpListenerRequest, string> method)
+        {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException("HTTP Listener is not supported on this computer.");
+                    
+ 
+            // URI prefixes are required, for example 
+            // "http://localhost:8080/index/".
+            if (address == null || address.Length == 0)
+                throw new ArgumentException("prefixes");
+ 
+            // A responder method is required
+            if (method == null)
+                throw new ArgumentException("method");
+ 
+            _listener.Prefixes.Add(address);
+ 
+            _responderMethod = method;
+            _listener.Start();
+        }
+        
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                string rstr = _responderMethod(ctx.Request);
+                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch { } // suppress any exceptions
+                            finally
+                            {
+                                // always close the stream
+                                ctx.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { } // suppress any exceptions
+            });
+        }
+ 
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+        
+        // Displays the header information that accompanied a request.
+        public static void DisplayWebHeaderCollection(HttpListenerRequest request)
+        {
+            System.Collections.Specialized.NameValueCollection headers = request.Headers;
+            // Get each header and display each value.
+            foreach (string key in headers.AllKeys)
+            {
+                string[] values = headers.GetValues(key);
+                if(values.Length > 0) 
+                {
+                    Console.WriteLine("The values of the {0} header are: ", key);
+                    foreach (string value in values) 
+                    {
+                        Console.WriteLine("   {0}", value);
+                    }
+                }
+                else
+                    Console.WriteLine("There is no value associated with the header.");
+            }
+        }
+    }
+}
